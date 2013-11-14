8ad5698d53d23271ab7e0e909dffc9046cd6267a
周 龙 <zhoulong0116@gmail.com>
2013-11-14 19:18:20 +0800

    增加Api单元测试案例

--- xfgj/Assets/Scripts/Common/Param.cs
+++ xfgj/Assets/Scripts/Common/Param.cs
@@ -6,9 +6,14 @@ public class Param
     public const string TOKEN = "token";
     public const string SCENE_ID = "scene_id";
     public const string SCENE_TYPE_ID = "scene_type_id";
+    public const string TYPE_ID = "type_id";
     public const string MODIFIED = "modified";
     public const string PRODUCT_ID = "product_id";
     public const string NUM_IID = "num_iid";
     public const string PRODUCER_ID = "producer_id";
-
+    public const string NAME = "name";
+    public const string PIC_URL = "pic_url";
+    public const string DETAILS = "details";
+    public const string CID = "cid";
+    public const string SIZE = "size";
 }
--- xfgj/Assets/Scripts/UnitTest/ApiTest.cs
+++ xfgj/Assets/Scripts/UnitTest/ApiTest.cs
@@ -26,7 +26,7 @@ public class ApiTest : UUnitTestCase
         GameObject obj = GameObject.Find("InitObj");
         ApiController.RequestParams rp = new ApiController.RequestParams();
         rp.data = new Dictionary<string, string>();
-        rp.data.Add(Param.SCENE_ID, 1);
+        rp.data.Add(Param.SCENE_ID, "100001");
         rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
         rp.callback = new ApiController.ResponseHandle(GetSceneCallback);
         obj.SendMessage("GetScene", rp, SendMessageOptions.RequireReceiver);
@@ -34,6 +34,153 @@ public class ApiTest : UUnitTestCase
 
     void GetSceneCallback(string res) {
         JsonData jd = JsonMapper.ToObject(res);
+        Scene scene = new Scene((int)(jd[Param.SCENE_ID]), (string)(jd[Param.NAME]),
+                                (int)(jd[Param.TYPE_ID]), (string)(jd[Param.PIC_URL]),
+                                (string)(jd[Param.DETAILS]), StringUtil.StringToDateTime((string)(jd[Param.MODIFIED])));
+        UUnitAssert.Equals(100001, scene.sceneId);
+    }
+
+    [UUnitTest]
+    public void GetScenesTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetScenesCallback);
+        obj.SendMessage("GetScenes", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetScenesCallback(string res) {
+        JsonData jd = JsonMapper.ToObject(res);
+        UUnitAssert.True(jd.IsArray);
+        Scene scene = new Scene((int)(jd[0][Param.SCENE_ID]), (string)(jd[0][Param.NAME]),
+                                (int)(jd[0][Param.TYPE_ID]), (string)(jd[0][Param.PIC_URL]),
+                                (string)(jd[0][Param.DETAILS]), StringUtil.StringToDateTime((string)(jd[0][Param.MODIFIED])));
+        UUnitAssert.NotNull(scene);
+    }
+
+    [UUnitTest]
+    public void GetSceneTypeTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.SCENE_TYPE_ID, "100001");
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetSceneTypeCallback);
+        obj.SendMessage("GetSceneType", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetSceneTypeCallback(string res) {
+        JsonData jd =JsonMapper.ToObject(res);
+        SceneType sceneType = new SceneType((int)(jd[Param.TYPE_ID]), (string)(jd[Param.NAME]),
+                                            StringUtil.StringToDateTime((string)(jd[Param.MODIFIED])));
+        UUnitAssert.Equals(100001, sceneType.typeId);
+    }
+
+    [UUnitTest]
+    public void GetAllSceneTypeTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetAllSceneTypeCallback);
+        obj.SendMessage("GetAllSceneType", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetAllSceneTypeCallback(string res) {
+        JsonData jd =JsonMapper.ToObject(res);
+        UUnitAssert.True(jd.IsArray);
+        SceneType sceneType = new SceneType((int)(jd[0][Param.TYPE_ID]), (string)(jd[0][Param.NAME]),
+                                            StringUtil.StringToDateTime((string)(jd[0][Param.MODIFIED])));
+        UUnitAssert.NotNull(sceneType);
+    }
+    
+    [UUnitTest]
+    public void GetProductInSceneTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.SCENE_ID, "100001");
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetProductInSceneCallback);
+        obj.SendMessage("GetProductInScene", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetProductInSceneCallback(string res) {
+        JsonData jd =JsonMapper.ToObject(res);
+        UUnitAssert.True(jd.IsArray);
+    }
+
+    [UUnitTest]
+    public void GetProductTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.PRODUCT_ID, "100001");
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetProductCallback);
+        obj.SendMessage("GetProduct", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetProductCallback(string res) {
+        JsonData jd =JsonMapper.ToObject(res);
+        UUnitAssert.True(!jd.IsArray);
+        Product product = new Product((int)(jd[Param.PRODUCT_ID]),
+                                      (int)(jd[Param.PRODUCER_ID]),
+                                      (string)(jd[Param.NAME]),
+                                      (int)(jd[Param.CID]),
+                                      (string)(jd[Param.SIZE]),
+                                      (string)(jd[Param.PIC_URL]),
+                                      (string)(jd[Param.DETAILS]),
+                                      StringUtil.StringToDateTime((string)(jd[Param.MODIFIED])));
+        UUnitAssert.NotNull(product);
+    }
+
+    [UUnitTest]
+    public void GetProducerTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.PRODUCER_ID, "100001");
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetProducerCallback);
+        obj.SendMessage("GetProducer", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetProducerCallback(string res) {
+        JsonData jd =JsonMapper.ToObject(res);
+        Producer producer = new Producer((int)(jd[Param.PRODUCER_ID]),
+                                        (string)(jd[Param.NAME]),
+                                        (string)(jd[Param.DETAILS]),
+                                        StringUtil.StringToDateTime((string)(jd[Param.MODIFIED])));
+        UUnitAssert.NotNull(producer);
+    }
+
+    [UUnitTest]
+    public void GetProductsWithProducerIdTest() {
+        GameObject obj = GameObject.Find("InitObj");
+        ApiController.RequestParams rp = new ApiController.RequestParams();
+        rp.data = new Dictionary<string, string>();
+        rp.data.Add(Param.PRODUCER_ID, "100001");
+        rp.data.Add(Param.TOKEN, AppSetting.getInstance().token);
+        rp.callback = new ApiController.ResponseHandle(GetProductsWithProducerIdCallback);
+        obj.SendMessage("GetProductsWithProducerId", rp, SendMessageOptions.RequireReceiver);
+    }
+
+    void GetProductsWithProducerIdCallback(string res) {
+        JsonData jd =JsonMapper.ToObject(res);
+        UUnitAssert.True(jd.IsArray);
+        if (jd.Count > 0) {
+            Product product = new Product((int)(jd[0][Param.PRODUCT_ID]),
+                                      (int)(jd[0][Param.PRODUCER_ID]),
+                                      (string)(jd[0][Param.NAME]),
+                                      (int)(jd[0][Param.CID]),
+                                      (string)(jd[0][Param.SIZE]),
+                                      (string)(jd[0][Param.PIC_URL]),
+                                      (string)(jd[0][Param.DETAILS]),
+                                      StringUtil.StringToDateTime((string)(jd[0][Param.MODIFIED])));
+            UUnitAssert.NotNull(product);
+        }
     }
 }
 
